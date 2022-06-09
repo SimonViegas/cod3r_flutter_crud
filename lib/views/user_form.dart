012e60f1c7f011d0dd1f022382a89954f04b420f
Simon.Viegas <simonviegas@outlook.com.br>
2022-06-09 20:51:01 -0300

    Criado Formulário de Usuário

--- lib/views/user_form.dart
+++ lib/views/user_form.dart
@@ -6,9 +6,34 @@ class UserForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Formulário de usuário'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Formulário de Usuário'),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.save),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Form(
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Nome'),
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Email'),
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'URL do Avatar'),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
