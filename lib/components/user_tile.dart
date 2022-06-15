33f1e6f3811e9ed7e96c7cdf4772d7d7ebd13e9f
Simon.Viegas <simonviegas@outlook.com.br>
2022-06-15 00:38:06 -0300

    Remoção de itens utilizando o Future

--- lib/components/user_tile.dart
+++ lib/components/user_tile.dart
@@ -16,58 +16,64 @@ class UserTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final avatar = user!.avatarUrl.isEmpty
-        ? const CircleAvatar(child: Icon(Icons.person))
-        : CircleAvatar(backgroundImage: NetworkImage(user!.avatarUrl));
+        ? const CircleAvatar(
+            child: Icon(Icons.person),
+          )
+        : CircleAvatar(
+            backgroundImage: NetworkImage(user!.avatarUrl),
+          );
     return ListTile(
-        leading: avatar,
-        title: Text(user!.name),
-        subtitle: Text(user!.email),
-        trailing: SizedBox(
-          width: 100,
-          child: Row(
-            children: [
-              IconButton(
-                icon: const Icon(Icons.edit),
-                color: Colors.orange,
-                onPressed: () {
-                  Navigator.of(context).pushNamed(
-                    AppRoutes.USER_FORM,
-                    arguments: user,
-                  );
-                },
-              ),
-              IconButton(
-                icon: const Icon(Icons.delete),
-                color: Colors.red,
-                onPressed: () {
-                  showDialog(
-                    context: context,
-                    builder: (ctx) => AlertDialog(
-                        title: const Text('Excluir usuário'),
-                        content: const Text('Tem certeza???'),
-                        actions: <Widget>[
-                          // ignore: deprecated_member_use
-                          FlatButton(
-                            child: const Text('Não'),
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                          ),
-                          // ignore: deprecated_member_use
-                          FlatButton(
-                            child: const Text('Sim'),
-                            onPressed: () {
-                              Provider.of<Users>(context, listen: false)
-                                  .remove(user);
-                              Navigator.of(context).pop();
-                            },
-                          ),
-                        ]),
-                  );
-                },
-              ),
-            ],
-          ),
-        ));
+      leading: avatar,
+      title: Text(user!.name),
+      subtitle: Text(user!.email),
+      trailing: SizedBox(
+        width: 100,
+        child: Row(
+          children: [
+            IconButton(
+              icon: const Icon(Icons.edit),
+              color: Colors.orange,
+              onPressed: () {
+                Navigator.of(context).pushNamed(
+                  AppRoutes.USER_FORM,
+                  arguments: user,
+                );
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.delete),
+              color: Colors.red,
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (ctx) => AlertDialog(
+                    title: const Text('Excluir usuário'),
+                    content: const Text('Tem certeza???'),
+                    actions: <Widget>[
+                      // ignore: deprecated_member_use
+                      FlatButton(
+                        child: const Text('Não'),
+                        onPressed: () => Navigator.of(context).pop(false),
+                      ),
+                      // ignore: deprecated_member_use
+                      FlatButton(
+                        child: const Text('Sim'),
+                        onPressed: () => Navigator.of(context).pop(true),
+                      ),
+                    ],
+                  ),
+                ).then((confirmed) => {
+                      if (confirmed)
+                        {
+                          Provider.of<Users>(context, listen: false)
+                              .remove(user),
+                        }
+                    });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
